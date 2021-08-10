74c96c29a4c1a84dd3d125d32eaf5640c8fa779f
lwy2108 <telegram @lwy01>
2021-08-11 01:00:12 +0800

    Added skeleton of fin-cal.py

---
+++ fin-cal.py
@@ -0,0 +1,56 @@
+"""
+fin-cal.py: A simple program for financial events.
+"""
+
+
+class Data:
+    pass
+
+
+class Event:
+
+    def __init__(self, date, time, description, sentiment):
+        self.date = date
+        self.time = time
+        self.description = description
+        self.sentiment = sentiment
+
+
+class Web:
+    pass
+
+
+def restore_data():
+    pass
+
+
+def add_event():
+    pass
+
+
+def scrape():
+    pass
+
+
+def edit_event():
+    pass
+
+
+def delete_event():
+    pass
+
+
+def backup_data():
+    pass
+
+
+def list_upcoming():
+    pass
+
+
+def list_all():
+    pass
+
+
+def notify():
+    pass
